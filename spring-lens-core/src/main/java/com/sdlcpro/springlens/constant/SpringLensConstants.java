5a6618626c2dcf507a39388199017ebd5e28f331
mamun <aalmamun295@gmail.com>
2026-08-31 20:10:00 +0600

    Add configurable feature to include beans from spring framework modules

--- spring-lens-core/src/main/java/com/sdlcpro/springlens/constant/SpringLensConstants.java
+++ spring-lens-core/src/main/java/com/sdlcpro/springlens/constant/SpringLensConstants.java
@@ -1,7 +1,5 @@
 package com.sdlcpro.springlens.constant;
 
-import java.util.Set;
-
 public final class SpringLensConstants {
 
     private SpringLensConstants() {
@@ -9,28 +7,4 @@ public final class SpringLensConstants {
     }
 
     public static final String SPRING_LENS_BASE_PACKAGE_PATTERN = "com.sdlcpro.springlens.**";
-
-    public static final Set<String> SPRING_FRAMEWORK_BASE_PACKAGE_PATTERNS = Set.of(
-            "org.springframework.aop.**",
-            "org.springframework.beans.**",
-            "org.springframework.context.**",
-            "org.springframework.core.**",
-            "org.springframework.expression.**",
-            "org.springframework.instrument.**",
-            "org.springframework.jdbc.**",
-            "org.springframework.jndi.**",
-            "org.springframework.orm.**",
-            "org.springframework.oxm.**",
-            "org.springframework.r2dbc.**",
-            "org.springframework.test.**",
-            "org.springframework.transaction.**",
-            "org.springframework.util.**",
-            "org.springframework.validation.**",
-            "org.springframework.web.**",
-            "org.springframework.web.reactive.**",
-            "org.springframework.web.servlet.**",
-            "org.springframework.web.socket.**"
-    );
-
-    public static final String SPRING_BOOT_BASE_PACKAGE_PATTERN = "org.springframework.boot.**";
 }
